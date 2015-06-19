44acb51d67fb14d00be989a85f7181631e8895ee
Francis Tseng <f+accounts@frnsys.com>
2015-06-18 22:34:19 -0400

    whew just forgot to specify a directory...

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='port',
-    version='0.0.5',
+    version='0.0.7',
     description='lightweight static blog',
     url='https://github.com/ftzeng/port',
     author='Francis Tseng (@frnsys)',
     license='MIT',
 
     zip_safe=True,
-    package_data={'': ['themes']},
+    package_data={'': ['port/themes']},
     include_package_data=True,
     packages=find_packages(),
     install_requires=[
