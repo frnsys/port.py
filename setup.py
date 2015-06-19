b9c672336b795eacb9854ee725e0558658a99109
Francis Tseng <f+accounts@frnsys.com>
2015-06-18 22:29:28 -0400

    pypi issues?

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='port',
-    version='0.0.4',
+    version='0.0.5',
     description='lightweight static blog',
     url='https://github.com/ftzeng/port',
     author='Francis Tseng (@frnsys)',
     license='MIT',
 
     zip_safe=True,
+    package_data={'': ['themes']},
+    include_package_data=True,
     packages=find_packages(),
     install_requires=[
         'click',
