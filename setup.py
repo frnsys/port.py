09b84d41052ce21ef2f65e191de22c40ed751a64
Francis Tseng <f+accounts@frnsys.com>
2015-07-08 10:15:38 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='port',
-    version='0.0.10',
+    version='0.1.0',
     description='lightweight static blog',
     url='https://github.com/ftzeng/port',
     author='Francis Tseng (@frnsys)',
